b01cc1f47ff3de384fac584cd91d88db093a9a46
Takács Alex <takacs.alex@united-consult.hu>
2026-01-23 23:49:09 +0100

    create background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Background from "./assets/background/background"
+
 function App() {
-  return
+  return(
+    <>
+      <div style={{ width: '99%', height: '99%', position: 'absolute'}}>
+      <Background
+          particleColors={["#eae7e7"]}
+          particleCount={200}
+          particleSpread={20}
+          speed={0.05}
+          particleBaseSize={100}
+          moveParticlesOnHover
+          alphaParticles={false}
+          disableRotation={false}
+          pixelRatio={1}
+      />
+      </div>
+    </>
+  );
 }
 
 export default App;
