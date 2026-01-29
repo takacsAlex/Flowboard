a4cd1f0d002d81562a25cdc79096e035216ccedb
Takács Alex <takacs.alex@united-consult.hu>
2026-01-29 20:20:01 +0100

    create sign up form

---
+++ src/pages/auth/auth.tsx
@@ -0,0 +1,12 @@
+import RegisterForm from '../../components/auth/registerForm.jsx';
+import './auth.css';
+
+const AuthPage = () => {
+    return(
+        <>
+            <RegisterForm />
+        </>
+    );
+}
+
+export default AuthPage;
