a815dc050eb20f59aa4d297a49e7b1be97c8bfd7
Takács Alex <lxtak08@gmail.com>
2026-01-30 09:22:27 +0100

    create signin form

---
+++ src/components/auth/loginForm.jsx
@@ -0,0 +1,41 @@
+import './loginForm.css'
+
+const LoginForm = () => {
+    return(
+        <>
+            <div className="container login_form">
+                <div className="row">
+                    <div className="col">
+                        <h1 id="signin_heading">Log into your account!</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <form>
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label" for="email">Email:</label>
+                                    <input type="text" id="email" className="form-control btn btn-outline-dark" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <label className="form-label" for="password">Password:</label>
+                                    <input type="password" id="password" className="form-control btn btn-outline-dark" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <a href="#" id="link_login">Don't have an account?</a>
+                            </div>
+                            <div className="row">
+                                <input type="submit" value={"Log in"} className="btn btn-outline-dark" id="submit"/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default LoginForm;
--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,10 +1,11 @@
 import RegisterForm from '../../components/auth/registerForm.jsx';
+import LoginForm from '../../components/auth/loginForm.jsx';
 import './auth.css';
 
 const AuthPage = () => {
     return(
         <>
-            <RegisterForm />
+            <LoginForm />
         </>
     );
 }
