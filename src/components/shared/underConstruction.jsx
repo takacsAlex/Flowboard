1d186020bc1a181e97f3bb7adbef53750e082ddb
Takács Alex <lxtak08@gmail.com>
2026-02-05 19:18:31 +0100

    create under-costruction widget (#9)

---
+++ src/components/shared/underConstruction.jsx
@@ -0,0 +1,9 @@
+import "./underConstruction.css"
+
+const UnderConstructionWidget = () => {
+    return(
+        <div className="container"><h1>This page is under construction!</h1></div>
+    );
+}
+
+export default UnderConstructionWidget;
