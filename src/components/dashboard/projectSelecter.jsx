3b91bae00a324c367505786e13e7cb01830417da
Takács Alex <lxtak08@gmail.com>
2026-02-28 18:41:58 +0100

    Create project selecter in dashboard (#14)

---
+++ src/components/dashboard/projectSelecter.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './projectSelecter.css'
+
+const ProjectSelecter = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selected, setSelected] = useState("Select your project:");
+    const options = [
+        "Main",
+        "Project1",
+        "Project2",
+        "Project3"
+    ];
+    
+    const handleSelect = (option) => {
+        setSelected(option + ":");
+        setIsOpen(false);
+    };
+
+    return(
+        <div className="projectSelecterModule">
+            <div class="select-box">
+            <div class={`options-container ${isOpen ? "active" : ""}`}>
+            {options.map((option, index) => (
+                <div
+                    key={index}
+                    className="option"
+                    onClick={() => handleSelect(option)}
+                >
+                    <label>{option}</label>
+                </div>
+            ))}
+            </div>
+            <div class="selected" onClick={() => setIsOpen(!isOpen)}>
+                {selected}
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default ProjectSelecter;
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,10 +1,16 @@
 import ProfileComponent from "../../components/dashboard/profile";
+import ProjectSelecter from "../../components/dashboard/projectSelecter";
 import "./dashboard.css";
 
 const Dashboard = () => {
     return(
         <>
-            <ProfileComponent />
+            <div className="row">
+                <div className="col"><ProfileComponent /></div>
+            </div>
+            <div className="row">
+                <div className="col"><ProjectSelecter /></div>
+            </div>
         </>
     );
 }
